9b1f0e61b56b20b4ee9da8b974d8b8e30475a617
Gurvinder Singh <gurvinder.singh@uninett.no>
2018-05-22 21:59:18 +0200

    merge notebook and spark docker images into one dockerfil.
    Now we are sure that notebook and spark runs the same version and makes it easier for a user to install the additional packages and have it available on all relevant nodes.

---
+++ jupyter-spark/start-master.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Strict mode
+set -euo pipefail
+
+echo "spark.deploy.recoveryDirectory ${SPARK_RECOVERY_DIR:-/tmp}" >> $SPARK_HOME/conf/spark-defaults.conf
+echo "spark.ui.reverseProxyUrl  https://${SPARK_PUBLIC_DNS}" >> $SPARK_HOME/conf/spark-defaults.conf
+
+unset SPARK_MASTER_PORT
+
+# Run spark-class directly so that when it exits (or crashes), the pod restarts.
+$SPARK_HOME/bin/spark-class org.apache.spark.deploy.master.Master --webui-port 8080
