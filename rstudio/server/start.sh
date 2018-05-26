7fb982aedf536e935e61c98e40b693bc2ab8a803
Gurvinder Singh <gurvinder.singh@uninett.no>
2018-05-26 07:58:36 +0200

    make sure when pvc is specified use that as backend for shiny apps

--- rstudio/server/start.sh
+++ rstudio/server/start.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-# Strict mode
-set -euo pipefail
+set -e
 
 echo "Starting RSudio Server"
 /usr/lib/rstudio-server/bin/rserver --server-daemonize 0 --auth-none 0 &
+
+if [ -n "$SHINY_APPS_PATH" ]; then
+	echo "Updating Shiny server directory path"
+	cp -r /srv/shiny-server/* $SHINY_APPS_PATH
+	rm -rf /srv/shiny-server
+	ln -s $SHINY_APPS_PATH /srv
+fi
+
 echo "Starting Shiny Server"
 shiny-server > /var/log/shiny-server/server.log &
 
