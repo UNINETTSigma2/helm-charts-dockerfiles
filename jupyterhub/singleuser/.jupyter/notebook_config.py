9e07686d8db72ce186220e5b1856e48fc9550613
Gurvinder Singh <gurvinder.singh@uninett.no>
2018-07-01 20:08:13 +0200

    added single user jupyterhub notebook with packages and exxtensions which can be useful

---
+++ jupyterhub/singleuser/.jupyter/notebook_config.py
@@ -0,0 +1,17 @@
+# Configuration file for ipython-notebook.
+
+c = get_config()
+
+# ------------------------------------------------------------------------------
+# NotebookApp configuration
+# ------------------------------------------------------------------------------
+
+c.IPKernelApp.pylab = 'inline'
+c.NotebookApp.ip = '*'
+c.NotebookApp.open_browser = False
+c.NotebookApp.port = 8888
+c.NotebookApp.base_url = '/'
+c.NotebookApp.trust_xheaders = True
+c.NotebookApp.tornado_settings = {'static_url_prefix': '/static/'}
+c.NotebookApp.notebook_dir = '/home/notebook'
+c.NotebookApp.allow_origin = '*'
