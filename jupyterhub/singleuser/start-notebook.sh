f31579d7f15e406fef03acfe295e73a01a4716a7
Gurvinder Singh <gurvinder.singh@uninett.no>
2018-07-03 10:50:42 +0200

    add support for symlinking the data folder if mounted

--- jupyterhub/singleuser/start-notebook.sh
+++ jupyterhub/singleuser/start-notebook.sh
@@ -13,4 +13,9 @@ if [ ! -d "$HOME/.jupyter" ]; then
 	cp -r /opt/.jupyter $HOME/.jupyter
 fi
 
+# If we have shared data mounted, the link it to current directory to have it visible in notebook
+if [ -d "/mnt/data" ]; then
+	ln -sf /mnt/data $HOME/data
+fi
+
 jupyterhub-singleuser --config $HOME/.jupyter/notebook_config.py
