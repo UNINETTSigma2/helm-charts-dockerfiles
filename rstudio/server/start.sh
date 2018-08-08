998449eb09c6f04698c4f35c9bcb4d60c2448891
Pål Karlsrud <paulkarlsrud@gmail.com>
2018-08-08 10:33:52 +0200

    Ensure that the shiny sample apps are properly copied to the PVC.

--- rstudio/server/start.sh
+++ rstudio/server/start.sh
@@ -8,6 +8,10 @@ echo "Starting RSudio Server"
 if [ -n "$SHINY_APPS_PATH" ]; then
 	echo "Updating Shiny server directory path"
 	cp -r /srv/shiny-server/* $SHINY_APPS_PATH
+	rm $SHINY_APPS_PATH/sample-apps
+	rm $SHINY_APPS_PATH/index.html
+	cp -r /opt/shiny-server/samples/sample-apps $SHINY_APPS_PATH/sample-apps
+	cp  /opt/shiny-server/samples/welcome.html $SHINY_APPS_PATH/index.html
 	rm -rf /srv/shiny-server
 	ln -s $SHINY_APPS_PATH /srv
 fi
