0d3f89d0391a60407b838ea5d99cf42ba44c3d57
Pål Karlsrud <paulkarlsrud@gmail.com>
2018-08-10 15:25:05 +0200

    Run the master and worker daemon with some special java options.

--- jupyter-spark/start-worker.sh
+++ jupyter-spark/start-worker.sh
@@ -5,10 +5,10 @@ set -euo pipefail
 
 unset SPARK_MASTER_PORT
 
-cores="${SPARK_WORKER_CORES:-1}"
-if [[ $cores == *m ]]; then
-    cores="${cores/%?/}"
-    cores="$((($cores + 999) / 1000))"
+core_limit="${SPARK_WORKER_CORES:-1}"
+if [[ $core_limit == *m ]]; then
+    core_limit="${core_limit/%?/}"
+    core_limit="$((($core_limit + 999) / 1000))"
 fi
 
 mem="${SPARK_WORKER_MEMORY:-1G}"
@@ -28,10 +28,10 @@ do
 		break
 	fi
 done
-
+export SPARK_DAEMON_JAVA_OPTS="-XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:ParallelGCThreads=${core_limit} -XX:ConcGCThreads=${core_limit} -Djava.util.concurrent.ForkJoinPool.common.parallelism=${core_limit}"
 
 export SPARK_DAEMON_MEMORY="$daemon_mem"
 # Run spark-class directly so that when it exits (or crashes), the pod restarts.
 $SPARK_HOME/bin/spark-class org.apache.spark.deploy.worker.Worker --webui-port 8081 \
-	 -c $cores -m $executor_mem -d ${SPARK_WORKER_DIRS:-/tmp/spark-worker} \
+	 -c $core_limit -m $executor_mem -d ${SPARK_WORKER_DIRS:-/tmp/spark-worker} \
 	 "spark://${SPARK_MASTER_SERVICE_HOST:-spark-master}:${SPARK_MASTER_SERVICE_PORT:-7077}"
