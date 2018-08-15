450421d20b9b9f42fb5b5cc4ab3ba6e447109980
Gurvinder Singh <gurvinder.singh@uninett.no>
2018-08-15 17:47:33 +0200

    check if the directory exist, if not copy the initial example. otherwise just create the symlink to it

--- rstudio/server/start.sh
+++ rstudio/server/start.sh
@@ -6,20 +6,19 @@ echo "Starting RSudio Server"
 /usr/lib/rstudio-server/bin/rserver --server-daemonize 0 --auth-none 0 &
 
 if [ -n "$SHINY_APPS_PATH" ]; then
-	echo "Updating Shiny server directory path"
-	if [ ! -e $SHINY_APPS_PATH/sample-apps ]; then
-	    cp -r /srv/shiny-server/* $SHINY_APPS_PATH
-	fi
-
+	if [ ! -d "$SHINY_APPS_PATH" ]; then
+		echo "Initialising Shiny App directory"
+		cp -r /srv/shiny-server/* $SHINY_APPS_PATH
         if [ -L $SHINY_APPS_PATH/sample-apps ]; then
-	    rm -rf $SHINY_APPS_PATH/sample-apps
-	    cp -r /opt/shiny-server/samples/sample-apps $SHINY_APPS_PATH/sample-apps
-	fi
+		    rm -rf $SHINY_APPS_PATH/sample-apps
+		    cp -r /opt/shiny-server/samples/sample-apps $SHINY_APPS_PATH/sample-apps
+		fi
         if [ -L $SHINY_APPS_PATH/index.html ]; then
-	    rm $SHINY_APPS_PATH/index.html
-	    cp /opt/shiny-server/samples/welcome.html $SHINY_APPS_PATH/index.html
+		    rm $SHINY_APPS_PATH/index.html
+		    cp /opt/shiny-server/samples/welcome.html $SHINY_APPS_PATH/index.html
+		fi
 	fi
-
+	echo "Updating Shiny server directory path"
 	rm -rf /srv/shiny-server
 	ln -s $SHINY_APPS_PATH /srv
 fi
