e0a7653ecc81cad32ad472726354336315b905bf
Pål Karlsrud <paulkarlsrud@gmail.com>
2019-09-11 17:39:47 +0200

    Ensure that the notebook config is always present.

--- jupyterhub/singleuser/start-notebook.sh
+++ jupyterhub/singleuser/start-notebook.sh
@@ -13,6 +13,10 @@ if [ ! -d "$HOME/.jupyter" ]; then
 	cp -r /opt/.jupyter $HOME/.jupyter
 fi
 
+if [ ! -f "$HOME/.jupyter/notebook_config.py" ]; then
+	cp -r /opt/.jupyter/notebook_config.py $HOME/.jupyter
+fi
+
 # If we have shared data mounted, the link it to current directory to have it visible in notebook
 if [ -d "/mnt/data" ]; then
 	rm -f $HOME/data
