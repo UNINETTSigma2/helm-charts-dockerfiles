c7f73524b860b7bf0bebdb99bc6d463a63e4a1d6
Pål Karlsrud <paulkarlsrud@gmail.com>
2019-09-20 17:17:23 +0200

    Use a newer version of websockify, which fixes a issue related to client protocol support.

--- desktop-vnc/src/common/install/no_vnc.sh
+++ desktop-vnc/src/common/install/no_vnc.sh
@@ -7,7 +7,7 @@ echo "Install noVNC - HTML5 based VNC viewer"
 mkdir -p $NO_VNC_HOME/utils/websockify
 wget -O- https://github.com/novnc/noVNC/archive/v1.0.0.tar.gz | tar xz --strip 1 -C $NO_VNC_HOME
 # use older version of websockify to prevent hanging connections on offline containers, see https://github.com/ConSol/docker-headless-vnc-container/issues/50
-wget -O- https://github.com/novnc/websockify/archive/v0.6.1.tar.gz | tar xz --strip 1 -C $NO_VNC_HOME/utils/websockify
+wget -O- https://github.com/novnc/websockify/archive/v0.9.0.tar.gz | tar xz --strip 1 -C $NO_VNC_HOME/utils/websockify
 chmod +x -v $NO_VNC_HOME/utils/*.sh
 
 cp $INST_SCRIPTS/vnc_lite.html $NO_VNC_HOME/index.html
