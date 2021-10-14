9d30db34589da2c871216738928241d9c3b1cd54
Siri Kallhovd <saw009@it0225548.klientdrift.uib.no>
2021-10-14 14:48:06 +0200

    try to download tigervnc

--- desktop-vnc/src/ubuntu/install/tigervnc.sh
+++ desktop-vnc/src/ubuntu/install/tigervnc.sh
@@ -2,4 +2,6 @@
 set -e
 
 echo "Install TigerVNC server"
-wget -O- https://dl.bintray.com/tigervnc/stable/tigervnc-1.8.0.x86_64.tar.gz | tar xz --strip 1 -C /
+wget -O- https://sourceforge.net/projects/tigervnc/files/stable/tigervnc-1.11.0.x86_64.tar.gz | tar xz --strip 1 -C /
+
+
