8e9d757cf79fa84ab7be4bdbe531e1fdc7f82add
Thierry Toutain <thierry.toutain@usit.uio.no>
2021-11-01 10:20:10 +0100

    use nc

--- build.sh
+++ build.sh
@@ -27,7 +27,7 @@ function build_image() {
     fi
 }
 
-for rawd in $(ls -d jupyterhub*/)
+for rawd in $(ls -d jupyter-spark*/)
 do
     directory=$(echo $rawd|sed 's/\///')
     if test -f "$directory/Dockerfile"
