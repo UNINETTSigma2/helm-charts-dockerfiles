186719f49cf866fd37972c42116b832eb0aa0dd1
Thierry Toutain <thierry.toutain@usit.uio.no>
2021-11-08 15:09:37 +0100

    remove chmod

--- desktop-vnc/src/common/install/no_vnc.sh
+++ desktop-vnc/src/common/install/no_vnc.sh
@@ -8,6 +8,6 @@ mkdir -p $NO_VNC_HOME/utils/websockify
 wget -O- https://github.com/novnc/noVNC/archive/v1.3.0.tar.gz | tar xz --strip 1 -C $NO_VNC_HOME
 # use older version of websockify to prevent hanging connections on offline containers, see https://github.com/ConSol/docker-headless-vnc-container/issues/50
 wget -O- https://github.com/novnc/websockify/archive/v0.10.0.tar.gz | tar xz --strip 1 -C $NO_VNC_HOME/utils/websockify
-chmod +x -v $NO_VNC_HOME/utils/*.sh
+#chmod +x -v $NO_VNC_HOME/utils/*.sh
 
 cp $INST_SCRIPTS/vnc_lite.html $NO_VNC_HOME/index.html
