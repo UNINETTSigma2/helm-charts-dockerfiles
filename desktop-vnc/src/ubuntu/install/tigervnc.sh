b32b627aa400cf695fc09966e84d37b1e3d4b253
Thierry Toutain <thierry.toutain@usit.uio.no>
2021-11-09 15:55:51 +0100

    add python

--- desktop-vnc/src/ubuntu/install/tigervnc.sh
+++ desktop-vnc/src/ubuntu/install/tigervnc.sh
@@ -4,4 +4,4 @@ set -e
 echo "Install TigerVNC server"
 
 #wget -O- https://dl.bintray.com/tigervnc/stable/tigervnc-1.8.0.x86_64.tar.gz | tar xz --strip 1 -C /
-wget -O- https://downloads.sourceforge.net/project/tigervnc/stable/1.11.0/tigervnc-1.11.0.x86_64.tar.gz | tar xz --strip 1 -C /
+wget -O- https://downloads.sourceforge.net/project/tigervnc/stable/1.10.0/tigervnc-1.10.0.x86_64.tar.gz | tar xz --strip 1 -C /
