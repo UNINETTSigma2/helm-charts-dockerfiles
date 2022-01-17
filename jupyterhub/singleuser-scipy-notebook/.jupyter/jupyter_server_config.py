0f0b0da482113a9c01b573fc0da328a34fc778da
andreasskeidsvoll <andreas.skeidsvoll@uib.no>
2022-01-17 10:44:19 +0100

    ServerApp and root_dir instead of deprecated

--- jupyterhub/singleuser-scipy-notebook/.jupyter/jupyter_server_config.py
+++ jupyterhub/singleuser-scipy-notebook/.jupyter/jupyter_server_config.py
@@ -8,13 +8,13 @@ c = get_config()
 # ------------------------------------------------------------------------------
 
 c.IPKernelApp.pylab = 'inline'
-c.NotebookApp.ip = '*'
-c.NotebookApp.quit_button = False
-c.NotebookApp.port = 8888
-c.NotebookApp.notebook_dir = os.environ['HOME']
-c.NotebookApp.allow_origin = '*'
-c.NotebookApp.allow_remote_access = True
-c.NotebookApp.token = ''
-c.NotebookApp.password = ''
+c.ServerApp.ip = '*'
+c.ServerApp.quit_button = False
+c.ServerApp.port = 8888
+c.ServerApp.root_dir = os.environ['HOME']
+c.ServerApp.allow_origin = '*'
+c.ServerApp.allow_remote_access = True
+c.ServerApp.token = ''
+c.ServerApp.password = ''
 # Run all nodes interactively
 c.InteractiveShell.ast_node_interactivity = "all"
