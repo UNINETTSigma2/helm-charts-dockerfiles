915abb68b2cdbd74809ebeacc16c9d2e95d8be4d
andreasskeidsvoll <andreas.skeidsvoll@uib.no>
2022-01-19 08:12:16 +0100

    Default spawner url to HOME/dirs

--- jupyterhub/singleuser/start-notebook.sh
+++ jupyterhub/singleuser/start-notebook.sh
@@ -56,7 +56,7 @@ fi
 
 cd "$HOME"
 if [[ ! -z "${JUPYTER_ENABLE_LAB}" ]]; then
-  jupyterhub-singleuser --config "$HOME/.jupyter/jupyter_server_config.py" --Spawner.default_url="/lab"
+  jupyterhub-singleuser --config "$HOME/.jupyter/jupyter_server_config.py" --Spawner.default_url="$HOME/lab"
 else
-  jupyterhub-singleuser --config "$HOME/.jupyter/jupyter_server_config.py" --Spawner.default_url="/tree"
+  jupyterhub-singleuser --config "$HOME/.jupyter/jupyter_server_config.py" --Spawner.default_url="$HOME/tree"
 fi
