b616085ab55f26c4fc47abca3c77080b2f726b81
andreasskeidsvoll <andreas.skeidsvoll@uib.no>
2022-02-01 09:59:05 +0100

    Run without privilege

--- rstudio/server/start.sh
+++ rstudio/server/start.sh
@@ -10,7 +10,7 @@ if [ ! -e "$HOME/.Renviron" ]; then
 fi
 
 #/usr/lib/rstudio-server/bin/rserver --server-daemonize 0 --session-timeout-minutes=0 --auth-none=0 --auth-timeout-minutes=0 --auth-stay-signed-in-days=30 &
-sudo rstudio-server start --session-timeout-minutes=0 --auth-timeout-minutes=0 --auth-stay-signed-in-days=30 &
+rstudio-server start --session-timeout-minutes=0 --auth-timeout-minutes=0 --auth-stay-signed-in-days=30 &
 
 if [ -n "$SHINY_APPS_PATH" ]; then
 	if [ ! -d "$SHINY_APPS_PATH" ]; then
