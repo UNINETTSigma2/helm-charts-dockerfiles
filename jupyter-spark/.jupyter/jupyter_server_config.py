39abf9909dc542ee7ef27d474fe9f22e166c02f6
andreasskeidsvoll <andreas.skeidsvoll@uib.no>
2022-02-07 10:41:11 +0100

    Try with ServerApp and extra server config

--- jupyter-spark/.jupyter/jupyter_server_config.py
+++ jupyter-spark/.jupyter/jupyter_server_config.py
@@ -3,19 +3,21 @@
 c = get_config()
 
 # ------------------------------------------------------------------------------
-# NotebookApp configuration
+# ServerApp configuration
 # ------------------------------------------------------------------------------
 
 c.IPKernelApp.pylab = 'inline'
-c.NotebookApp.ip = '*'
-c.NotebookApp.open_browser = False
-c.NotebookApp.quit_button = False
-c.NotebookApp.port = 8888
-c.NotebookApp.base_url = '/'
-c.NotebookApp.trust_xheaders = True
-c.NotebookApp.tornado_settings = {'static_url_prefix': '/static/'}
-c.NotebookApp.notebook_dir = '/home/notebook'
-c.NotebookApp.allow_origin = '*'
-c.NotebookApp.allow_remote_access = True
+c.ServerApp.ip = '*'
+c.ServerApp.open_browser = False
+c.ServerApp.quit_button = False
+c.ServerApp.port = 8888
+c.ServerApp.base_url = '/'
+c.ServerApp.trust_xheaders = True
+c.ServerApp.tornado_settings = {'static_url_prefix': '/static/'}
+c.ServerApp.notebook_dir = '/home/notebook'
+c.ServerApp.allow_origin = '*'
+c.ServerApp.allow_remote_access = True
+c.ServerApp.token = ''
+c.ServerApp.password = ''
 # Run all nodes interactively
 c.InteractiveShell.ast_node_interactivity = 'all'
