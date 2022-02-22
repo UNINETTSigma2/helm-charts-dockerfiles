957c5df52efcd2277c3d3f66f223906addb2c973
andreasskeidsvoll <andreas.skeidsvoll@uib.no>
2022-02-22 08:39:19 +0100

    Also add user to env

--- rstudio/server/start.sh
+++ rstudio/server/start.sh
@@ -4,6 +4,7 @@ set -e
 
 echo "Starting RSudio Server"
 echo "session-default-working-dir=/home/$USERNAME" >> /etc/rstudio/rsession.conf
+echo "USER=$USER" >> /etc/environment
 
 if [ ! -e "$HOME/.Renviron" ]; then
 	echo -e "HOME=/home/$USERNAME\nUSER=$USERNAME\nTZ=Europe/Oslo" > "$HOME/.Renviron"
