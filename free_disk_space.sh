01dc10bee84a2d20c265bf68c40ff56ea8e139b1
Siri Kallhovd <47630846+sirikal@users.noreply.github.com>
2023-10-10 14:31:01 +0200

    testing updated version of the free_disk_space.sh

--- free_disk_space.sh
+++ free_disk_space.sh
@@ -20,11 +20,13 @@
 # Total space: 85GB
 # Allocated: 67 GB
 # Free: 17 GB
-# This script frees up 28 GB of disk space by deleting unneeded packages and 
+# This script frees up 28 GB of disk space by deleting unneeded packages and
 # large directories.
 # The Flink end to end tests download and generate more than 17 GB of files,
 # causing unpredictable behavior and build failures.
 #
+# Origin: https://github.com/apache/flink/blob/master/tools/azure-pipelines/free_disk_space.sh 10.10.2023
+
 echo "=============================================================================="
 echo "Freeing up disk space on CI system"
 echo "=============================================================================="
@@ -32,21 +34,23 @@ echo "==========================================================================
 echo "Listing 100 largest packages"
 dpkg-query -Wf '${Installed-Size}\t${Package}\n' | sort -n | tail -n 100
 df -h
-whereis dotnet
-which dotnet
 echo "Removing large packages"
-sudo apt-get remove -y '^ghc-8.*'
-sudo apt-get remove -y '^ghc-9.*'
 sudo apt-get remove -y '^dotnet-.*'
 sudo apt-get remove -y '^llvm-.*'
 sudo apt-get remove -y 'php.*'
-sudo apt-get remove -y azure-cli google-cloud-sdk hhvm google-chrome-stable firefox powershell mono-devel microsoft-edge-stable
+sudo apt-get remove -y '^mongodb-.*'
+sudo apt-get remove -y '^mysql-.*'
+sudo apt-get remove -y azure-cli google-cloud-cli hhvm google-chrome-stable firefox powershell mono-devel microsoft-edge-stable libgl1-mesa-dri
 sudo apt-get autoremove -y
 sudo apt-get clean
 df -h
 echo "Removing large directories"
-# deleting 15GB
-rm -rf /usr/share/dotnet/
-rm -rf /usr/lib/dotnet/
-rm -rf /usr/local/share/dotnet/
+
+sudo rm -rf /usr/share/dotnet/
+sudo rm -rf /usr/local/graalvm/
+sudo rm -rf /usr/local/.ghcup/
+sudo rm -rf /usr/local/share/powershell
+sudo rm -rf /usr/local/share/chromium
+sudo rm -rf /usr/local/lib/android
+sudo rm -rf /usr/local/lib/node_modules
 df -h
