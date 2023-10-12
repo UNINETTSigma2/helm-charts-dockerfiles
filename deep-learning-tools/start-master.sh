b7282f9d7f77e782c4e7841a45f4476ca4f8671e
Siri Kallhovd <47630846+sirikal@users.noreply.github.com>
2023-10-12 15:01:04 +0200

    building the full dlt in one place

---
+++ deep-learning-tools/start-master.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Strict mode
+set -euo pipefail
+
+echo "spark.deploy.recoveryDirectory ${SPARK_RECOVERY_DIR:-/tmp/spark-master}" >> $SPARK_HOME/conf/spark-defaults.conf
+echo "spark.ui.reverseProxyUrl  https://${SPARK_PUBLIC_DNS}" >> $SPARK_HOME/conf/spark-defaults.conf
+
+core_limit="${SPARK_DAEMON_CORES:-1}"
+if [[ $core_limit == *m ]]; then
+    core_limit="${core_limit/%?/}"
+    core_limit="$((($core_limit + 999) / 1000))"
+fi
+
+unset SPARK_MASTER_PORT
+export SPARK_DAEMON_JAVA_OPTS="-XX:+UnlockExperimentalVMOptions -XX:+UseContainerSupport -XX:ParallelGCThreads=${core_limit} -XX:ConcGCThreads=${core_limit} -Djava.util.concurrent.ForkJoinPool.common.parallelism=${core_limit}"
+
+# Run spark-class directly so that when it exits (or crashes), the pod restarts.
+$SPARK_HOME/bin/spark-class org.apache.spark.deploy.master.Master --webui-port 8080
---
+++ deep-learning-tools/start-worker.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Strict mode
+set -euo pipefail
+
+unset SPARK_MASTER_PORT
+
+core_limit="${SPARK_WORKER_CORES:-1}"
+if [[ $core_limit == *m ]]; then
+    core_limit="${core_limit/%?/}"
+    core_limit="$((($core_limit + 999) / 1000))"
+fi
+echo "spark.ui.reverseProxyUrl  https://${SPARK_PUBLIC_DNS}" >> $SPARK_HOME/conf/spark-defaults.conf
+
+mem="${SPARK_WORKER_MEMORY:-1g}"
+read daemon_mem executor_mem < <(mem_parser.py $mem 0.4 1000M)
+
+# This is to give JAVA process a head start to have master up and running
+echo "Waiting for Master: ${SPARK_MASTER_SERVICE_HOST:-spark-master}:${SPARK_MASTER_SERVICE_PORT:-7077}"
+
+# Do infinite loop as k8s will killus if master is not up anyway
+while true
+do
+	if ! `nc -z -w5 "${SPARK_MASTER_SERVICE_HOST:-spark-master}" "${SPARK_MASTER_SERVICE_PORT:-7077}"`;
+	then
+		echo "Master: ${SPARK_MASTER_SERVICE_HOST:-spark-master} is not up. I am early, retrying.."
+	else
+		echo "Master: ${SPARK_MASTER_SERVICE_HOST:-spark-master} is up !"
+		break
+	fi
+done
+export SPARK_DAEMON_JAVA_OPTS="-XX:+UnlockExperimentalVMOptions -XX:+UseContainerSupport -XX:ParallelGCThreads=${core_limit} -XX:ConcGCThreads=${core_limit} -Djava.util.concurrent.ForkJoinPool.common.parallelism=${core_limit}"
+
+export SPARK_DAEMON_MEMORY="$daemon_mem"
+# Run spark-class directly so that when it exits (or crashes), the pod restarts.
+$SPARK_HOME/bin/spark-class org.apache.spark.deploy.worker.Worker --webui-port 8081 \
+	 -c $core_limit -m $executor_mem -d ${SPARK_WORKER_DIRS:-/tmp/spark-worker} \
+	 "spark://${SPARK_MASTER_SERVICE_HOST:-spark-master}:${SPARK_MASTER_SERVICE_PORT:-7077}"
