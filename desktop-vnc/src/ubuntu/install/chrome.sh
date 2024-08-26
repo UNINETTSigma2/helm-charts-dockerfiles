c51c6eba470e861b5afb1c6b6bdefc1a28fa4c6e
Johan Magnus Engevik <johan.magnus.engevik@uib.no>
2024-08-26 13:46:47 +0200

    Removed references to firefox and chrome. Removed desktop folder with firefox and chrome

--- desktop-vnc/src/ubuntu/install/chrome.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-### every exit != 0 fails the script
-set -e
-
-echo "Install Chromium Browser"
-ln -s /usr/bin/chromium-browser /usr/bin/google-chrome
-### fix to start chromium in a Docker container, see https://github.com/ConSol/docker-headless-vnc-container/issues/2
-echo "CHROMIUM_FLAGS='--no-sandbox --start-maximized --user-data-dir'" > $HOME/.chromium-browser.init
